a0aa27e0271f6d7262955f3e8c7ba6d53b519fb3
winpanw <winstonpanw@gmail.com>
2024-07-08 12:35:50 +0100

    abc pack

--- Packs/abc/Scripts/abc/abc.py
+++ Packs/abc/Scripts/abc/abc.py
@@ -1,8 +1,9 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+
+
 def main():
     try:
         win_test()
     except Exception as e:
         print(str(e))
-    #    print(traceback.format_exc())
